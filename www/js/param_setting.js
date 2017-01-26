19ea5b005f7b30c68906e5bd79529fc8ce9257d8
bedford <bedford2001@gmail.com>
2017-01-26 09:28:01 +0800

    param_setting_io - load io status with ajax response informations
    param_setting_io -
    
     <1>将DO状态CSS类移到do-status类下面，成为子类;
     <2>增加初始化IO和串口参数的JS操作函数;
     <3>增加DO显示状态切换的jquery操作函数;
     <4>删除协议类型和串口波特率options值;初始化DO为关闭状态;

--- www/js/param_setting.js
+++ www/js/param_setting.js
@@ -25,7 +25,10 @@ function init_network_param() {
 }
 
 /* 系统时间设置相关js */
-function addzero(v) {if (v < 10) return '0' + v;return v.toString();}
+function addzero(v) {
+	if (v < 10) return '0' + v;
+	return v.toString();
+}
 
 function update_pc_time() {
 	var date = new Date();
@@ -70,7 +73,7 @@ function checkLeave() {
 function display_snmp_param(snmp_param) {
 	var html = "";
 	for (var i in snmp_param.authority_ip) {
-		if (snmp_param.authority_ip[i].valid_flag == "1") {
+		if (snmp_param.authority_ip[i].valid_flag == 1) {
 			html += '<li class="snmp_item"><span class="fix_span"><input type="checkbox" onchange="update(\''
                 +i+'\')" checked="checked"></span>';
 		} else {
@@ -103,8 +106,74 @@ function load_snmp_param() {
             });
 }
 
+/* 串口及IO口状态及设置相关 */
+$(function() {
+	$('.do-status li').click(function() {
+        var is_on = $(this).hasClass('do-status-on');
+        if (is_on) {
+            $(this).removeClass('do-status-on');
+            $(this).addClass('do-status-off');
+        } else {
+            $(this).removeClass('do-status-off');
+            $(this).addClass('do-status-on');
+        }
+    });
+});
+
+function display_io_param(io_param) {
+    for (var i in io_param.uart_param) {
+    	$('#rs232_baudrate_select').append('<option value="'+ io_param.uart_param[i].value
+			+ '">' + io_param.uart_param[i].text + '</option>');
+		$('#rs485_baudrate_select').append('<option value="'+ io_param.uart_param[i].value
+			+ '">' + io_param.uart_param[i].text + '</option>');
+	}
+
+	for (var i in io_param.protocol_param) {
+	    $('#rs232_protocol_select').append('<option value="'+ io_param.protocol_param[i].value
+			+ '">' + io_param.protocol_param[i].text + '</option>');
+		$('#rs485_protocol_select').append('<option value="'+ io_param.protocol_param[i].value
+			+ '">' + io_param.protocol_param[i].text + '</option>');
+	}
+
+	$('#rs232_protocol_select').val(io_param.rs232_protocol);
+	$('#rs232_baudrate_select').val(io_param.rs232_baudrate);
+
+	$('#rs485_protocol_select').val(io_param.rs485_protocol);
+	$('#rs485_baudrate_select').val(io_param.rs485_baudrate);
+
+	if (io_param.rs232_flag) {
+		$('#rs232_checkbox').attr("checked", true);
+	}
+
+	if (io_param.rs485_flag == 1) {
+		$('#rs485_checkbox').attr("checked", true);
+	}
+
+	$('.do-status li').each(function(i) {
+		if (io_param.io_status[i].value) {
+            $(this).removeClass('do-status-off');
+			$(this).addClass('do-status-on');
+		}
+	});
+}
+
+function load_io_param() {
+    var json = new Object();
+    json.msg_type = 0;
+    json.cmd_type = 2;
+    var data = $.toJSON(json);
+    $.ajax({
+                url     : "/cgi-bin/common.cgi",
+                type    : "POST",
+                dataType: "json",
+                data    : data,
+                success : function(msg) {
+                    display_io_param(msg);
+                },
+            });
+}
+
 function update(i) {
 	var input_name = "\'#authority_ip_" + i + "\'";
 	//$(input_name).attributes
 }
-
