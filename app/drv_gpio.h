ed341258496a2c98274ecd4eec47be1ef940c9b0
bedford <bedford2001@gmail.com>
2017-08-20 17:52:31 +0800

    drv_gpio - fix bug can not set DO_1
    drv_gpio -
    
     解决无法设置DO_1值的问题(蜂鸣器输出);

--- app/drv_gpio.h
+++ app/drv_gpio.h
@@ -5,23 +5,26 @@
 extern "C" {
 #endif
 
+/**
+ * @brief   GPIO序号枚举定义 
+ */
 enum GPIO_NAME {
-    DIGITAL_IN_0 = 0,
-    DIGITAL_IN_1,
-    DIGITAL_IN_2,
-    DIGITAL_IN_3,
+    DIGITAL_IN_0 = 0,   /* DI1 */
+    DIGITAL_IN_1,       /* DI2 */
+    DIGITAL_IN_2,       /* DI3 */
+    DIGITAL_IN_3,       /* DI4 */
 
-    DIGITAL_OUT_0,
-    DIGITAL_OUT_1,
-    DIGITAL_OUT_2,
-    DIGITAL_OUT_3,
+    DIGITAL_OUT_0,      /* DO1 */
+    DIGITAL_OUT_1,      /* DO2 */
+    DIGITAL_OUT_2,      /* DO3 */
+    DIGITAL_OUT_3,      /* DO4 */
 
-    POFF_PIN,
-    PD_INT_PIN,
+    POFF_PIN,           /* 关闭电池电源引脚 */
+    PD_INT_PIN,         /* 电池启动引脚 */
 
-    WATCHDOG_PIN,
+    WATCHDOG_PIN,       /* 外加的硬件看门狗喂狗引脚(已去除) */
 
-    RS485_ENABLE,
+    RS485_ENABLE,       /* RS485读\写切换使能引脚 */
 
     MAX_GPIO_NAME,
 };
@@ -35,9 +38,32 @@ enum GPIO_NAME {
 #define RET_ERR_WRITE       (-0x101)
 #define RET_ERR_READ        (-0x102)
 
+/**
+ * @brief   drv_gpio_open   打开指定序号GPIO
+ * @param   gpio_name       GPIO序号(见GPIO_NAME枚举)
+ * @return
+ */
 int drv_gpio_open(unsigned int gpio_name);
+
+/**
+ * @brief   drv_gpio_write  写GPIO指定引脚状态
+ * @param   gpio_name       GPIO序号
+ * @param   val             电平值(0, 1)
+ */
 int drv_gpio_write(unsigned int gpio_name, unsigned char val);
+
+/**
+ * @brief   drv_gpio_read   读GPIO指定引脚状态
+ * @param   gpio_name       GPIO序号
+ * @param   pval            电平值(0, 1)
+ */
 int drv_gpio_read(unsigned int gpio_name, unsigned char *pval);
+
+/**
+ * @brief   drv_gpio_close  关闭指定GPIO 
+ * @param   gpio_name       GPIO序号
+ * @return
+ */
 int drv_gpio_close(unsigned int gpio_name);
 
 #ifdef __cplusplus
