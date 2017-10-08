6cea43e5510e3f167fe091617880759c81217117
bedford <bedford2001@gmail.com>
2017-10-08 14:31:59 +0800

    alarm_param_manage.html - support more device type selection and change query history data time limit
    alarm_param_manage.html -
    
     <1>解决设备选择的异常;
     <2>调整历史数据查询的结束时间限制;

--- www/js/query_history.js
+++ www/js/query_history.js
@@ -3,12 +3,13 @@ function addzero(v) {if (v < 10) return '0' + v;return v.toString();}
 
 function load_timepicker_default() {
 	var date = new Date();
+	date.setHours(date.getHours() + 1);
 	var end_time = date.getFullYear().toString() + '-' + addzero(date.getMonth() + 1) + '-'
 			+ addzero(date.getDate()) + ' ' + addzero(date.getHours()) + ':'
 			+ addzero(date.getMinutes()) + ':' + addzero(date.getSeconds());
 	$('#end_time').val(end_time);
 
-	date.setHours(date.getHours() - 1);
+	date.setHours(date.getHours() - 2);
 	var start_time = date.getFullYear().toString() + '-' + addzero(date.getMonth() + 1) + '-'
 			+ addzero(date.getDate()) + ' ' + addzero(date.getHours()) + ':'
 			+ addzero(date.getMinutes()) + ':' + addzero(date.getSeconds());
