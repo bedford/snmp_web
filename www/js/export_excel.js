42e85db45b333444ba955e5afd97fcf7fb9b59e8
bedford <bedford2001@gmail.com>
2017-10-15 20:36:58 +0800

    js/export_excel.js - change table export to excel plugin
    js/export_excel.js -
    
     <1>调整表格导出成excel的插件;
     <2>删除查询数据的缓存功能;
     <3>合并表头和表格内容;

--- www/js/export_excel.js
+++ www/js/export_excel.js
@@ -1,63 +1,101 @@
-function JSONToExcelConvertor(JSONData, FileName, Title) {
-    //先转化json
-    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
-    var excel = '<table>';
+(function($){
+    $.fn.extend({
+        tableExport: function(options) {
+            var defaults = {
+                    separator: ',',
+                    ignoreColumn: [],
+                    tableName:'yourTableName',
+                    escape:'true',
+                    htmlContent:'false',
+                    fileName: 'data_record',
+            };
 
-    //设置表头
-    var row = "<tr>";
-    for (var i = 0, l = Title.length; i < l; i++) {
-        row += "<td>" + Title[i] + '</td>';
-    }
-    //换行
-    excel += row + "</tr>";
+            var options = $.extend(defaults, options);
+            var el = this;
 
-    //设置数据
-    for (var i = 0; i < arrData.length; i++) {
-        var row = "<tr>";
-        for (var index in arrData[i]) {
-			var value = arrData[i][index] === "." ? "" : arrData[i][index];
-            row += '<td>' + value + '</td>';
+            var excel="<table>";
+            // Header
+            $(el).find('thead').find('tr').each(function() {
+                excel += "<tr>";
+                $(this).filter(':visible').find('th').each(function(index,data) {
+                    if ($(this).css('display') != 'none') {
+                        if(defaults.ignoreColumn.indexOf(index) == -1) {
+                            excel += "<td>'" +parseString($(this))+ "</td>";
+                        }
+                    }
+                });
+                excel += '</tr>';
+            });					
+            
+            // Row Vs Column
+            var rowCount=1;
+            $(el).find('tbody').find('tr').each(function() {
+                excel += "<tr>";
+                var colCount=0;
+                $(this).filter(':visible').find('td').each(function(index,data) {
+                    if ($(this).css('display') != 'none'){	
+                        if(defaults.ignoreColumn.indexOf(index) == -1){
+                            excel += "<td>'"+parseString($(this))+"</td>";
+                        }
+                    }
+                    colCount++;
+                });															
+                rowCount++;
+                excel += '</tr>';
+            });					
+            excel += '</table>'
+
+            var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:"+defaults.type+"' xmlns='http://www.w3.org/TR/REC-html40'>";
+            excelFile += "<head>";
+            excelFile += "<!--[if gte mso 9]>";
+            excelFile += "<xml>";
+            excelFile += "<x:ExcelWorkbook>";
+            excelFile += "<x:ExcelWorksheets>";
+            excelFile += "<x:ExcelWorksheet>";
+            excelFile += "<x:Name>";
+            excelFile += "工作表1";
+            excelFile += "</x:Name>";
+            excelFile += "<x:WorksheetOptions>";
+            excelFile += "<x:DisplayGridlines/>";
+            excelFile += "</x:WorksheetOptions>";
+            excelFile += "</x:ExcelWorksheet>";
+            excelFile += "</x:ExcelWorksheets>";
+            excelFile += "</x:ExcelWorkbook>";
+            excelFile += "</xml>";
+            excelFile += "<![endif]-->";
+            excelFile += "</head>";
+            excelFile += "<body>";
+            excelFile += excel;
+            excelFile += "</body>";
+            excelFile += "</html>";
+
+            /*var base64data = "base64," + $.base64.encode(excelFile);
+            window.open('data:application/vnd.ms-'+defaults.type+';filename=exportData.doc;' + base64data);*/
+            var uri = 'data:application/vnd.ms-excel;charset=utf-8,' + encodeURIComponent(excelFile);
+            var link = document.createElement("a");
+            link.href = uri;
+        
+            link.style = "visibility:hidden";
+            link.download = defaults.fileName + ".xls";
+
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            function parseString(data) {
+                if (defaults.htmlContent == 'true') {
+                    content_data = data.html().trim();
+                } else {
+                    content_data = data.text().trim();
+                }
+
+                if (defaults.escape == 'true') {
+                    content_data = escape(content_data);
+                }
+
+                return content_data;
+            }
+
         }
-
-        excel += row + "</tr>";
-    }
-    excel += "</table>";
-
-    var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'>";
-    excelFile += '<meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">';
-    excelFile += '<meta http-equiv="content-type" content="application/vnd.ms-excel';
-    excelFile += '; charset=UTF-8">';
-    excelFile += "<head>";
-    excelFile += "<!--[if gte mso 9]>";
-    excelFile += "<xml>";
-    excelFile += "<x:ExcelWorkbook>";
-    excelFile += "<x:ExcelWorksheets>";
-    excelFile += "<x:ExcelWorksheet>";
-    excelFile += "<x:Name>";
-    excelFile += "工作表1";
-    excelFile += "</x:Name>";
-    excelFile += "<x:WorksheetOptions>";
-    excelFile += "<x:DisplayGridlines/>";
-    excelFile += "</x:WorksheetOptions>";
-    excelFile += "</x:ExcelWorksheet>";
-    excelFile += "</x:ExcelWorksheets>";
-    excelFile += "</x:ExcelWorkbook>";
-    excelFile += "</xml>";
-    excelFile += "<![endif]-->";
-    excelFile += "</head>";
-    excelFile += "<body>";
-    excelFile += excel;
-    excelFile += "</body>";
-    excelFile += "</html>";
-
-    var uri = 'data:application/vnd.ms-excel;charset=utf-8,' + encodeURIComponent(excelFile);
-    var link = document.createElement("a");
-    link.href = uri;
-
-    link.style = "visibility:hidden";
-    link.download = FileName + ".xls";
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
+    });
+})(jQuery);
